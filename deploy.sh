9b3a7b542d004c8a090519e86333df3cb89b9467
Daniel Yinanc <noemails@noemails.com>
2019-01-26 07:20:37 -0500

    Fixed Deploy typos

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
 then
-    ./terraform init
-    ./terraform validate
-    ./terraform plan
-    ./terraform apply -auto-approve
+    terraform init
+    terraform validate
+    terraform plan
+    terraform apply -auto-approve
 fi
